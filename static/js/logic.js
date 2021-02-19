24909ebdba36b6935b64ae25c4a6800847a46ada
CicilyGeorge <69083251+CicilyGeorge@users.noreply.github.com>
2021-02-20 02:09:45 +0800

    template edit

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,25 +4,29 @@ function displayLineGraph_Production(Data, countryName) {
     // Filter production Data to return only data for matching country name
     let productionData = Data.filter(production => production.Attribute == "Production");
     let countryProduction = productionData.filter(country => country.Country_Name == countryName);
+    countryProduction = countryProduction.filter(country => country.Year >= 1990);
 
-    // console.log(countryProduction);
+    let years = [];
+    let productionValues = [];
 
+    for (let i=0; i < countryProduction.length; i++) {
     // // Extract years and production values from data 
-    let years = Object.values(countryProduction['Year']);
-    let productionValues = Object.values(countryProduction['Values']);
+        years.push(countryProduction[i].Year);
+        productionValues.push(countryProduction[i].Value);
+    }
 
     console.log(years)
     console.log(productionValues)
     // // Select the first 10 years of data for the actual population graph
     // // Convert year string to number
     // // Plot with both lines and markers for each data point
-    // let lineData_actual = {
-    //     x: populationYears.slice(0,10).map(i => Number(i)),
-    //     y: populationAmounts.slice(0,10),
-    //     name: "Actual",
-    //     type: "scatter",
-    //     mode: "lines+markers"
-    // };
+    let lineData_actual = {
+        x: years,
+        y: productionValues,
+        name: "Actual",
+        type: "scatter",
+        mode: "lines+markers"
+    };
       
     // // Select the last three years of data for the predicted population graph
     // // Need to select the last data point of the actual population so graphs
@@ -40,17 +44,17 @@ function displayLineGraph_Production(Data, countryName) {
     // };
 
     // // Place both data sets together in array
-    // let lineData = [lineData_actual, lineData_predicted];
+    let lineData = [lineData_actual];  // , lineData_predicted
 
-    // // Set title for line graph and x and y axes
-    // let lineLayout = {
-    //      title: countryName + " - Population Actual and Predicted 1970 to 2050",
-    //      xaxis: { title: "Years" },
-    //      yaxis: { title: "Population (thousands(k)/ millions(M)/ billions(B))" }
-    // };
+    // Set title for line graph and x and y axes
+    let lineLayout = {
+         title: countryName + " - Coffee Production Actual and Predicted 1970 to 2050",
+         xaxis: { title: "Years" },
+         yaxis: { title: "Production (thousands(k)/ millions(M)/ billions(B))" }
+    };
     
     // // Use plotly to display line graph at div ID "line2" with lineData and lineLayout
-    // Plotly.newPlot('line1', lineData, lineLayout);
+    Plotly.newPlot('prodLine', lineData, lineLayout);
 }
 
 
